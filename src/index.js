277762d613f7088f4b73d47642b82bd139d1475f
SimKitkat <simone.cornelius14@gmail.com>
2024-09-03 17:29:08 +0200

    css and js update

--- src/index.js
+++ src/index.js
@@ -95,4 +95,29 @@ function cityName(event) {
 let searchFormElement = document.querySelector("#search-Form");
 searchFormElement.addEventListener("submit", cityName);
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#weather-forecast-week");
+  forecastElement.innerHTML = forecastHtml;
+}
 searchCity("windhoek");
+displayForecast();
